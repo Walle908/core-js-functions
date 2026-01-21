d2febb66978c81fa73ed66bd85e37098b6409f40
Walle908 <walle0908@yandex.ru>
2026-01-21 22:57:58 +0400

    feat: implement solution for getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -228,8 +228,13 @@ function partialUsingArguments(fn, ...argsBound) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom - 1;
+
+  return () => {
+    count += 1;
+    return count;
+  };
 }
 
 module.exports = {
